5f186e8e3363d238fb0e3f0591a7c211717ff548
Görkem Öztürk <gorkemozturkse@gmail.com>
2021-01-25 15:14:38 +0300

    Initial domain layout that is including person and contact classes has been implemented.

---
+++ SimpleDirectory.Domain/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimpleDirectory.Domain.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
---
+++ SimpleDirectory.Domain/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimpleDirectory.Domain.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+
+        // Relations
+        public virtual ICollection<Contact> Contacts { get; set; }
+    }
+}
